45034f9204d04d9ae0ea8aa24a3c71a3c9c0c74b
Aritana Noara <arinoara@yahoo.com.br>
2021-07-19 21:28:36 -0300

    exercicio01:done

--- starwars.js
+++ starwars.js
@@ -1,4 +1,13 @@
 // Seu javascript aqui :)
+import { play } from "./music-sem-private.js"
+const musicConfiguration = {
+    audioUrl: 'audio/tema-sw.mp3',
+    coverImageUrl: 'imgs/logo.svg',
+    title: 'Intro',
+    artist: 'John Williams'
+}
+play(musicConfiguration, document.body);
+
 // Use a Star Wars API: https://swapi.dev/
 // para fazer uma requisição assíncrona e:
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
